95aa39ea281a47e879cad38a91179a9996a8116d
Alek Kras <alekforwork@gmail.com>
2018-03-08 13:38:52 -0500

    First program

---
+++ hello_world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(2018, 3, 8, 11, 25, 0); //the exact date when I started learning C#
+            Console.WriteLine("The day when I started learning C# was on " + date1.ToString("d"));
+            Console.WriteLine("It was a sunny day outside but I was in the office learning C#");
+            Console.WriteLine("Then I looked at the watch and it was " + date1.ToString("HH:mm"));
+            Console.WriteLine("Oh, hello there!!!");
+            string name;
+            Console.Write("How shalleth I calleth thy?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}! I have been learning C# since {1}", name, date1.ToString("d"));
+        }
+    }
+}
