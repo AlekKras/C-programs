a4e5d0c0ee8744dde52f250e503d74648b350fec
root <alekforwork@gmail.com>
2018-06-12 13:58:29 -0400

    Added Classes program

---
+++ numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Aleks", 1000);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent Payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            Console.WriteLine(account.GetAccountHistory());
+            // testing that initial balance to be positive
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("exception caught");
+                Console.WriteLine(e.ToString());
+            }
+            // test for a negative balance
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+    }    
+}
